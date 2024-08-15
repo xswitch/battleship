82a7ffd90e7181cc927803d9f891841081697a45
xswitch <bivarmong@gmail.com>
2024-08-15 17:07:20 +0200

    Passed first test

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -2,6 +2,12 @@ class GameBoard {
   constructor() {
     this.placedCoords = [];
   }
+
+  placeShip(coords) {
+    if (this.placedCoords.includes(coords)) return false;
+    this.placedCoords.push(coords);
+    return true;
+  }
 }
 
 export default GameBoard;
--- src/js/gameboard.test.js
+++ src/js/gameboard.test.js
@@ -4,7 +4,7 @@ import GameBoard from "./gameboard";
 describe("Gameboard class", () => {
   it("should place Ships at coords", () => {
     const gameBoard = new GameBoard();
-    gameBoard.placeShip(1, 1);
+    gameBoard.placeShip([1, 1]);
     expect(gameBoard.placedCoords.length).toBe(1);
   });
 });
